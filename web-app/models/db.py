8f12328aaf6a7d1fb00f15471e29f8b1d6c353d4
chasecvitale <chasevitale@gmail.com>
2026-04-15 19:40:52 -0400

    edit database connection in db.py

--- web-app/models/db.py
+++ web-app/models/db.py
@@ -2,8 +2,16 @@
 Connects to database
 """
 
+import os
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client["diary_db"]
+# we need to make sure that the database is connected correctly with the
+# MongoDB database in the container
+mongo_uri = os.getenv("MONGO_URI", "mongodb://localhost:27017/")
+client = MongoClient(mongo_uri)
+
+db_name= os.getenv("DB_NAME", "crimson_viper")
+db = client[db_name]
+
+users_collection = db["users"]
 entries_collection = db["entries"]
