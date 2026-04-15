3844fdbeb222c51836a883f46353ac1ed177e2f5
chasecvitale <chasevitale@gmail.com>
2026-04-15 02:07:46 -0400

    added docstrings to entry_routes

--- web-app/routes/entry_routes.py
+++ web-app/routes/entry_routes.py
@@ -1,3 +1,7 @@
+"""
+App routes
+"""
+
 from flask import Blueprint, request, jsonify
 from services.entry_service import (
     add_task,
@@ -15,6 +19,7 @@ entry_bp = Blueprint("entries", __name__)
 
 @entry_bp.route("/entries", methods=["POST"])
 def create():
+    """Creates a new entry"""
     data = request.get_json(silent=True) or {}
     username = data.get("username")
     date = data.get("date")
@@ -31,11 +36,13 @@ def create():
 
 @entry_bp.route("/entries/<username>", methods=["GET"])
 def get_all(username):
+    """Gets all usernames"""
     return jsonify(get_all_entries(username))
 
 
 @entry_bp.route("/entries/<username>/<date>", methods=["GET"])
 def get_one(username, date):
+    """Gets one entry with username and date"""
     entry = get_entry_by_date(username, date)
     if not entry:
         return jsonify({"error": "Not found"}), 404
@@ -44,6 +51,7 @@ def get_one(username, date):
 
 @entry_bp.route("/entries/<username>/<date>/<int:entry_index>", methods=["PUT"])
 def update(username, date, entry_index):
+    """Updates an entry"""
     updated_data = request.get_json(silent=True) or {}
     result = update_entry(username, date, entry_index, updated_data)
     if not result:
@@ -53,6 +61,7 @@ def update(username, date, entry_index):
 
 @entry_bp.route("/entries/<username>/<date>/<int:entry_index>", methods=["DELETE"])
 def delete(username, date, entry_index):
+    """Deletes an entry"""
     result = delete_entry(username, date, entry_index)
     if not result:
         return jsonify({"error": "Entry not found"}), 404
@@ -61,6 +70,7 @@ def delete(username, date, entry_index):
 
 @entry_bp.route("/tasks/<username>/<date>", methods=["POST"])
 def create_task(username, date):
+    """Creates a new task"""
     task_data = request.get_json(silent=True) or {}
     result = add_task(username, date, task_data)
     return jsonify({"msg": "task added", "result": str(result)})
@@ -68,6 +78,7 @@ def create_task(username, date):
 
 @entry_bp.route("/tasks/<username>/<date>/<int:task_index>", methods=["PUT"])
 def update_task(username, date, task_index):
+    """Updates a task"""
     updated_task = request.get_json(silent=True) or {}
     result = edit_task(username, date, task_index, updated_task)
     if not result:
@@ -77,6 +88,7 @@ def update_task(username, date, task_index):
 
 @entry_bp.route("/tasks/<username>/<date>/<int:task_index>", methods=["DELETE"])
 def remove_task(username, date, task_index):
+    """Removes a task"""
     result = delete_task(username, date, task_index)
     if not result:
         return jsonify({"error": "Task not found"}), 404
