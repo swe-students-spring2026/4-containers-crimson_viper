13e38d33e545c91cb50911ff246fc4546e25542d
chasecvitale <chasevitale@gmail.com>
2026-04-16 00:48:37 -0400

    added recording audio for test

---
+++ web-app/routes/audio_routes.py
@@ -0,0 +1,40 @@
+"""
+Audio routes
+"""
+
+from datetime import datetime
+import os
+import uuid
+
+from flask import Blueprint, request, jsonify
+from flask_login import login_required, current_user
+
+from models.db import db
+
+audio_bp = Blueprint("audio", __name__)
+
+
+@audio_bp.route("/upload-audio", methods=["POST"])
+@login_required
+def upload_audio():
+    audio_file = request.files.get("audio")
+
+    filename = f"{uuid.uuid4()}.wav"
+    audio_dir = "/data/audio"
+
+    os.makedirs(audio_dir, exist_ok=True)
+
+    file_path = os.path.join(audio_dir, filename)
+    audio_file.save(file_path)
+
+    db.audio_jobs.insert_one({
+        "username": current_user.username,
+        "created_at": datetime.utcnow(),
+        "audio_path": file_path,
+        "status": "unprocessed",
+        "transcription": None,
+        "emotion": None
+    })
+
+    # we need to have a return statement for it to work
+    return jsonify({"message": "Audio uploaded successfully"}), 200
